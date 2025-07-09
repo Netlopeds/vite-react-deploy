96257216ecb341f19662046c137ff5c721a3a5f5
Netlopeds <mrproatmcorentiixm@gmail.com>
2025-07-09 20:40:11 +0800

    Fix Font v3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Import font directly
+import AgrandirFont from './Fonts/Agrandir-Regular.otf'
+
+// Create style element to inject font
+const style = document.createElement('style')
+style.textContent = `
+  @font-face {
+    font-family: 'Agrandir-Regular';
+    src: url('${AgrandirFont}') format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+`
+document.head.appendChild(style)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
